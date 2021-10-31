f7d837f614bb93801ef7e7b6103e6f60afaebcc1
Nico Hauser <me@tyratox.ch>
2021-10-31 21:12:16 +0100

    Update client.sh

--- setup/client.sh
+++ setup/client.sh
@@ -11,6 +11,20 @@ apt -y install sudo
 # and the networking setup (e.g. root certificate)
 ./networking.sh
 
+apt -y install libnss3-tools
+
+# now install the custom root certificate for firefox as well
+# see https://stackoverflow.com/a/2760345/2897827 and https://stackoverflow.com/a/48424709/2897827
+for certDB in $(find  ~/.mozilla* ~/.thunderbird -name "cert9.db")
+do
+  certDir=$(dirname ${certDB});
+  certutil -A -n "iMovies Root Certificate" -t "TCu,Cuw,Tuw" -i ./public-keys/web-root.crt -d sql:${certDir}
+done
+
+
+cp ./public-keys/web-root.crt /usr/local/share/ca-certificates/
+update-ca-certificates
+
 # set the ip of the computer
 sed -i 's/address 192.168.0.0/address 192.168.0.4/' /etc/network/interfaces
 
