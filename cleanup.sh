98d3dc22f409a834f5ed041d8a7bcd1929f691dc
Nico Hauser <me@tyratox.ch>
2021-10-31 20:23:09 +0100

    add client ip

--- cleanup.sh
+++ cleanup.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
+# uninstall git
+apt -y purge git
+
+# run autoremove
+apt -y autoremove
+
+# update packages
+apt -y update
+apt -y upgrade
+
 # delete old rotated/compressed logs
 find /var/log -type f -regex ".*\.gz$"
 find /var/log -type f -regex ".*\.[0-9]$"
 
 # empty all existing log files
 for i in $(find /var/log -type f); do cat /dev/null > $i; done
-
-# uninstall git
-apt -y purge git
-
-# run autoremove
-apt -y autoremove
 
 # reboot
 reboot
--- networking.sh
+++ networking.sh
@@ -4,6 +4,9 @@
 # SSH and DHCP is enabled by default and it is enabled automatically
 apt -y install firewalld
 
+# disable dhcp, we use static IPs
+firewall-cmd –permanent –remove-service=dhcpv6-client
+
 # now install the custom root certificate. extension must be .crt
 cp ./public-keys/web-root.crt /usr/local/share/ca-certificates/
 update-ca-certificates
@@ -12,6 +15,7 @@ update-ca-certificates
 echo "192.168.0.1 imovies.ch" >> /etc/hosts
 echo "192.168.0.2 ca.imovies.ch" >> /etc/hosts
 echo "192.168.0.3 backup.imovies.ch" >> /etc/hosts
+echo "192.168.0.4 client1.imovies.ch" >> /etc/hosts
 
 # change network interface name s.t. the first interface is called eth0
 sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="net.ifnames=0 biosdevname=0"/' /etc/default/grub
