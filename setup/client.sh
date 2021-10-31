dc27eac030ad043190ed9145b22c45a4327c468f
Nico Hauser <me@tyratox.ch>
2021-10-31 21:40:13 +0100

    Update client.sh

--- setup/client.sh
+++ setup/client.sh
@@ -10,9 +10,6 @@ apt -y install sudo
 
 # and the networking setup (e.g. root certificate)
 ./networking.sh
-# now install the custom root certificate for firefox as well
-# see https://bgstack15.wordpress.com/2018/10/04/firefox-trust-system-trusted-certificates/
-dirname $(grep -IrL 'p11-kit-trust.so' ~/.mozilla/firefox/*/pkcs11.txt) | xargs -t -d '\n' -I {} modutil -dbdir sql:{} -force -add 'PKCS #11 Trust Storage Module' -libfile /usr/lib64/pkcs11/p11-kit-trust.so
 
 
 cp ./public-keys/web-root.crt /usr/local/share/ca-certificates/
