a5e9e39a832248f8cbd93deda1b5179a95c6d898
Nico Hauser <me@tyratox.ch>
2021-11-01 23:04:14 +0100

    change network interfaces

--- setup/client.sh
+++ setup/client.sh
@@ -8,14 +8,20 @@
 # except for sudo
 apt -y install sudo
 
-# and the networking setup (e.g. root certificate)
-./networking.sh
-
-
+# now install the custom root certificate. extension must be .crt
 cp ./public-keys/web-root.crt /usr/local/share/ca-certificates/
 update-ca-certificates
 
+# setup dns using /etc/hosts
+echo "10.0.0.254 imovies.ch" >> /etc/hosts
+
+# change network interface name s.t. the first interface is called eth0
+sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="net.ifnames=0 biosdevname=0"/' /etc/default/grub
+grub-mkconfig -o /boot/grub/grub.cfg
+
+cat ./configs/client-interfaces.conf > /etc/network/interfaces
+
 # set the ip of the computer
-sed -i 's/address 192.168.0.0/address 192.168.0.4/' /etc/network/interfaces
+sed -i 's/address 10.0.0.0/address 10.0.0.1/' /etc/network/interfaces
 
 ./cleanup.sh
