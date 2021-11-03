5b59b2668abe831aad1b9aefd2e5b55b91bf8fc5
Nico Hauser <me@tyratox.ch>
2021-11-03 11:48:16 +0100

    Update backend.config.js

--- configs/pm2/backend.config.js
+++ configs/pm2/backend.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: "CA backend",
       cwd: "/opt/pm2/asl-ca-backend/",
-      script: "ts-node",
+      script: "/home/webapp/.yarn/bin/ts-node",
       args: "/opt/pm2/asl-ca-backend/src/main.ts",
     },
   ],
