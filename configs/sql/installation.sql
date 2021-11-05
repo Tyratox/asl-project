f5b8d75d24c0a2be77a6afddf85f03a1cae8aa80
Nico Hauser <me@tyratox.ch>
2021-11-05 23:30:31 +0100

    Update sql configuration

--- configs/sql/installation.sql
+++ configs/sql/installation.sql
@@ -1,8 +1,11 @@
 UPDATE mysql.user SET Password=PASSWORD('toor') WHERE User='root';
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'toor';
 DELETE FROM mysql.user WHERE User='';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 DROP DATABASE IF EXISTS test;
 DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
-FLUSH PRIVILEGES;
+
 CREATE DATABASE imovies;
+CREATE USER 'imovies'@'localhost' identified by 'toor';
+GRANT ALL PRIVILEGES ON imovies.* to 'imovies'@'localhost';
+
+FLUSH PRIVILEGES;
