f3c71b7d8e427dc3e300ee8ac3cb5a9f6e3a0c40
Nico Hauser <me@tyratox.ch>
2021-11-15 09:12:45 +0100

    Install gnutls for rsyslog

--- optional/log-receiver.sh
+++ optional/log-receiver.sh
@@ -4,6 +4,9 @@
 firewall-cmd --permanent --add-port=10514/tcp
 firewall-cmd --reload
 
+# allow ryslog to use tls
+apt-get -y install rsyslog-gnutls
+
 cat ./configs/rsyslog-receiver.conf > /etc/rsyslog.d/receiver.conf
 
 # restart
--- optional/log-sender.sh
+++ optional/log-sender.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# allow ryslog to use tls
+apt-get -y install rsyslog-gnutls
+
 cat ./configs/rsyslog-sender.conf > /etc/rsyslog.d/sender.conf
 
 systemctl restart rsyslog
