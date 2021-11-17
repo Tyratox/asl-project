5768de79dda610afe7417b95eecd2052d338bf68
Nico Hauser <me@tyratox.ch>
2021-11-17 21:23:43 +0100

    Update user-dir-auditing.sh

--- optional/user-dir-auditing.sh
+++ optional/user-dir-auditing.sh
@@ -7,7 +7,7 @@ mkdir -p "/home/$USER"
 # first create it if it doesn't exist yet
 touch "/home/$USER/.bash_history"
 # then change ownership
-chown "$USER:$USER /home/$USER/.bash_history"
+chown "$USER:$USER" "/home/$USER/.bash_history"
 # and change the permissions
 chmod 700 "/home/$USER/.bash_history"
 # Only the superuser or a process possessing the CAP_LINUX_IMMUTABLE capability can set or clear this attribute. 
