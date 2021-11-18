eb269b13ce09331bc7668662d706b998939fb736
Nico Hauser <me@tyratox.ch>
2021-11-18 17:27:59 +0100

    Update encrypt.cpp

--- backup/encrypt.cpp
+++ backup/encrypt.cpp
@@ -45,7 +45,7 @@ void mkdir(const char* dir){
       int devNull = open("/dev/null", O_WRONLY);
       dup2(devNull, STDERR_FILENO);
       // child process
-      execl(mkdirPath.c_str(), "-p", dir, NULL);
+      execl(mkdirPath.c_str(), "mkdir", "-p", dir, NULL);
       return;
   }
 }
@@ -67,8 +67,8 @@ string readFile(string file){
 }
 
 int main(int argc, char *argv[]){
-  if(argc < 8){
-    throw runtime_error("Usage: ./encrypt file dir action LOCAL_FOLDER_NAME LOCAL_enc_PATH CIPHER_MODE KEY_PATH");
+  if(argc < 9){
+    throw runtime_error("Usage: ./encrypt file dir action WATCH_DIR ENC_PATH TMP_PATH CIPHER_MODE KEY_PATH");
   }
 
   string dir_str = argv[1];
@@ -76,23 +76,26 @@ int main(int argc, char *argv[]){
 
   fs::path file(dir_str + file_str);
   string action = argv[3];
-  fs::path CA_PATH(argv[4]);
-
-  fs::path ENC_PATH(argv[5]);
-  string CIPHER_MODE = argv[6];
-  string KEY_PATH = argv[7];
   
-  fs::path relative_path = fs::relative(file, CA_PATH);
+  fs::path WATCH_DIR(argv[4]);
+  fs::path ENC_PATH(argv[5]);
+  fs::path TMP_PATH(argv[6]);
 
-  fs::path file_enc =  ENC_PATH / relative_path;
+  string CIPHER_MODE = argv[7];
+  string KEY_PATH = argv[8];
+  
+  fs::path relative_path = fs::relative(file, WATCH_DIR);
 
-  std::string ivPath = std::tmpnam(nullptr);
-  system(("openssl rand 16 -hex > " + ivPath).c_str());
+  fs::path file_enc =  ENC_PATH / (relative_path.append(".enc"));
+
+  fs::path ivPath = TMP_PATH / (relative_path.append(".IV"));
+  fs::path tmpEncPath = TMP_PATH / (relative_path.append(".enc"));
+
+  mkdir(ivPath.parent_path().c_str());
+  mkdir(tmpEncPath.parent_path().c_str());
+
+  system(("openssl rand 16 -hex > " + ivPath.generic_string()).c_str());
   string IV = readFile(ivPath);
-
-  std::string tmpEncPath = std::tmpnam(nullptr);
-
-  mkdir(file_enc.parent_path().c_str());
 
   pid_t c_pid = fork();
 
@@ -115,11 +118,11 @@ int main(int argc, char *argv[]){
           return 0;
       } else {
           // tar together IV and encrypted file
-          execl(tarPath.c_str(), "-cf", file_enc.c_str(), ivPath, tmpEncPath.c_str());
+          execl(tarPath.c_str(), "-cf", file_enc.c_str(), ivPath.c_str(), tmpEncPath.c_str());
           return 0;
       }
   } else {
-      execl(opensslPath.c_str(), "enc", ("-" + CIPHER_MODE).c_str(), "-a", "-A", "-iv", IV, "-in", file.c_str(), "-out", tmpEncPath.c_str(), "-e", "-k", KEY_PATH.c_str(), NULL);
+      execl(opensslPath.c_str(), "enc", ("-" + CIPHER_MODE).c_str(), "-a", "-A", "-iv", IV.c_str(), "-in", file.c_str(), "-out", tmpEncPath.c_str(), "-e", "-k", KEY_PATH.c_str(), NULL);
       return 0;
   }
 
