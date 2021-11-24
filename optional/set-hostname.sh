c9f95b2efc64f1dba94e4515922ab48967790e14
Nico Hauser <me@tyratox.ch>
2021-11-24 23:24:00 +0100

    Fix smaller issues in setup scripts

--- optional/set-hostname.sh
+++ optional/set-hostname.sh
@@ -3,6 +3,7 @@ HOSTNAME=$1
 
 hostnamectl set-hostname $HOSTNAME
 
+sed -i "s/hostname.imovies.ch\thostname/$HOSTNAME/" /etc/hosts
 sed -i "s/hostname/$HOSTNAME/" /etc/hosts
 # initial image had the hostname certificate-authority, just for backwards compatibility
 sed -i "s/certificate-authority/$HOSTNAME/" /etc/hosts
