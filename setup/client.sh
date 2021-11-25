85b23be0a6669d32387f9e3dfb8a783ffa5a7e0b
Nico Hauser <me@tyratox.ch>
2021-11-25 10:35:56 +0100

    Revert "Update client.sh"
    This reverts commit 3729895a67db0dbb0d25c61ed7d253cbad5085f0.

--- setup/client.sh
+++ setup/client.sh
@@ -29,4 +29,13 @@ echo "10.0.0.253 fw-2.imovies.ch" >> /etc/hosts
 
 echo "172.16.0.1 backup.imovies.ch" >> /etc/hosts
 
+# change network interface name s.t. the first interface is called eth0
+sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="net.ifnames=0 biosdevname=0"/' /etc/default/grub
+grub-mkconfig -o /boot/grub/grub.cfg
+
+cat ./configs/client-interfaces.conf > /etc/network/interfaces
+
+# set the ip of the computer
+sed -i 's/address 10.0.0.0/address 10.0.0.1/' /etc/network/interfaces
+
 ./cleanup.sh
