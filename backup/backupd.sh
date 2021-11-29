6b6d626b182e81d72a6c2fd133e5e3eebb54c153
Nico Hauser <me@tyratox.ch>
2021-11-29 21:50:10 +0100

    Delete user private key after backup

--- backup/backupd.sh
+++ backup/backupd.sh
@@ -3,7 +3,8 @@
 DIR_TO_BACKUP=$1
 REMOTE_HOST=$2
 REMOTE_DIR=$3
+AFTER_CMD=$4
 
 while true; do
-    find $DIR_TO_BACKUP | entr -d rsync -rltvh --chmod=ugo=rwX --delete -e ssh $DIR_TO_BACKUP $REMOTE_HOST:$REMOTE_DIR
+    find $DIR_TO_BACKUP | entr -d sh -c "rsync -rltvh --chmod=ugo=rwX -e ssh $DIR_TO_BACKUP $REMOTE_HOST:$REMOTE_DIR;$AFTER_CMD"
 done
