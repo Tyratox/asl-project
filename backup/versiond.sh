43b972fa74c4c5439e96ac9dd8eea5797d7e7649
Nico Hauser <me@tyratox.ch>
2021-11-30 16:37:08 +0100

    Update versiond.sh

--- backup/versiond.sh
+++ backup/versiond.sh
@@ -3,7 +3,7 @@
 DIR_TO_VERSION=$1
 VERSIONING_DIR=$2
 
-inotifywait -r -m "$DIR_TO_VERSION" -e modify |
+inotifywait -r -m "$DIR_TO_VERSION" -e modify -e move |
 while read dir action file; do
     rsync -avh --backup --backup-dir=backup_`date +%F-%H-%M` $DIR_TO_VERSION $VERSIONING_DIR
 done
