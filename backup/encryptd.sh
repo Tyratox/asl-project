9a0cbf9153d61aa3b329159695520293df4abe9e
Nico Hauser <me@tyratox.ch>
2021-11-30 15:35:27 +0100

    Update encryptd.sh

--- backup/encryptd.sh
+++ backup/encryptd.sh
@@ -7,7 +7,7 @@ TMP_PATH=$4
 CIPHER_MODE=$5
 KEY_PATH=$6
 
-inotifywait -r -m "$DIR_TO_MONITOR" -e modify |
+inotifywait -r -m "$DIR_TO_MONITOR" -e modify -e move |
 while read dir action file; do
     "$ENC_BINARY" "$dir" "$file" "$action" "$DIR_TO_MONITOR" "$ENC_DIR_TO_STORE" "$TMP_PATH" "$CIPHER_MODE" "$KEY_PATH"
 done
